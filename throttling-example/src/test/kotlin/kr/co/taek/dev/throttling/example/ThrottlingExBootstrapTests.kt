0f1d8d91a46a4e696d988d66e9630da46075abb8
tax1116 <tax941116@gmail.com>
2025-03-27 19:11:55 +0900

    Rename : packages

---
+++ throttling-example/src/test/kotlin/kr/co/taek/dev/throttling/example/ThrottlingExBootstrapTests.kt
@@ -0,0 +1,12 @@
+package kr.co.taek.dev.throttling.example
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ThrottlingExBootstrapTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
