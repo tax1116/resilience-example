ed0e728860aeb6bfbad01875b443aa00fe3936ed
tax1116 <tax941116@gmail.com>
2025-04-03 17:46:45 +0900

    주석 추가

--- throttling/bucket4j-example/src/main/kotlin/kr/co/taek/dev/throttling/bucket4j/example/ch4/ListenableRateLimiter.kt
+++ throttling/bucket4j-example/src/main/kotlin/kr/co/taek/dev/throttling/bucket4j/example/ch4/ListenableRateLimiter.kt
@@ -8,8 +8,12 @@ import java.time.Duration
 private val logger = KotlinLogging.logger {}
 
 /**
- * 간단한 토큰 버킷 RateLimiter를 테스트합니다.
+ * Bucket 에 Listener를 등록하는 예제
+ * 8.11.0 version 이후로는 withListener() 메소드를 통해 Listener를 등록할 수 있다.
+ * 8.10.1 version 이전에는 버킷을 만든 후에 toListener() 메소드를 통해 Listener를 등록할 수 있다.
  *
+ * 8.10.1 이전에는 bucket4j-core 라는 모듈을 사용
+ * 8.11.0 이후에는 jdk 버전에 따라 분리된 모듈을 사용
  */
 class ListenableRateLimiter {
     private val listener = SimpleBucketListener()
